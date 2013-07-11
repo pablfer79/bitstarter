61257007c46e4d8800f6f88bb1ca3a4df992f59e
Pablo Fernandez <pablfer79@hotmail.com>
2013-07-11 19:25:47 +0000

    Fix

--- web.js
+++ web.js
@@ -1,19 +1,12 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
 var fs = require('fs');
 
-
-
 app.get('/', function(request, response) {
-  var buf = fs.readFile('index.html');
-  if (!buf){
-      console.log("No hay buf");
-  } else {
-      response.send(fs.readFile(buf.toString());
-}
-});		   
+  var buffer = new Buffer();
+  
+  response.send(buffer.toString('utc-8', fs.readFileSync('index.html')));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
