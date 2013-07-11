942ff3201540dea65e0d398d4fb5bd85d92a1088
Pablo Fernandez <pablfer79@hotmail.com>
2013-07-11 18:40:15 +0000

    Added index.html message

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send(fs.readFile('index.html','utf8'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
